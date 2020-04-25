07f0b3330e62b8e969cc44a8d940e2ca5c52e69e
tpang29 <tpang29@gmail.com>
2020-04-25 14:49:21 -0400

    new tests

--- drivers/CriticalPathDriver.java
+++ drivers/CriticalPathDriver.java
@@ -10,18 +10,25 @@ public class CriticalPathDriver
         // Create project tasks
         TaskNode taskA = new TaskNode("A", 8);
         TaskNode taskB = new TaskNode("B", 6);
-        TaskNode taskC = new TaskNode("D", 4);
-        TaskNode taskD = new TaskNode("A", 7);
-        TaskNode taskE = new TaskNode("B", 3);
-        TaskNode taskF = new TaskNode("D", 2);
+        TaskNode taskC = new TaskNode("C", 4);
+        TaskNode taskD = new TaskNode("D", 7);
+        TaskNode taskE = new TaskNode("E", 3);
+        TaskNode taskF = new TaskNode("F", 2);
         TaskNode taskG = new TaskNode("G", 5);
 
         // Add successors for each task
-        taskA.addSuccessor(taskB);
-        taskA.addSuccessor(taskD);
+        taskA.addSuccessors(taskB, taskD);
 
-        taskB.addSuccessor(taskG)
+        taskB.addSuccessors(taskC, taskG);
 
-        // CriticalPath.calculateES(taskA);
+        taskC.addSuccessors(taskF);
+
+        taskD.addSuccessors(taskE, taskG);
+
+        taskE.addSuccessors(taskF);
+
+        taskG.addSuccessors(taskC, taskE, taskF);
+
+        CriticalPath.calculate(taskA, taskF);
     }
 }
